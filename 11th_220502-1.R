72551a78679dcbe51ce9d4a1131818c718045888
James SungJun PARK <psj482@gmail.com>
2022-05-09 17:55:30 +0900

    Rmd practice

---
+++ 11th_220502-1.R
@@ -0,0 +1,27 @@
+install.packages("rmarkdown")
+install.packages("prettydoc")
+install.packages("epiDisplay")
+
+# HTML table
+install.packages("kableExtra")
+library(kableExtra)
+
+# An R interface to the DataTables library
+install.packages("DT")
+
+
+# How to Knit to PDF
+
+#No LaTeX installation detected (LaTeX is required to create PDF output). 
+
+#You should install a LaTeX distribution for your platform: https://www.latex-project.org/get/
+
+#If you are not sure, you may install TinyTeX in R: tinytex::install_tinytex()
+#tinytex::install_tinytex()
+
+#Otherwise consider MiKTeX on Windows - http://miktex.org
+
+#MacTeX on macOS - https://tug.org/mactex/
+#  (NOTE: Download with Safari rather than Chrome _strongly_ recommended)
+
+#Linux: Use system package manager
